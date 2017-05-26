164a7f9b1a015a9808cb49d4273e3759abf3d432
Vikram Patil <vikram.vvp@gmail.com>
2017-05-26 16:11:27 +0530

    added project

---
+++ app/index.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+// state
+// lifecycle events
+// UI (always required)
+
+class App extends React.Component {
+    render() {
+        return (
+            <div> 
+                Hello React Training!
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
